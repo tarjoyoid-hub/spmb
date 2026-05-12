c572bc8ab1f75124b42447e9b7714a46b91e8880
tarjoyoid-hub <tarjoyo.id@gmail.com>
2026-05-12 15:13:39 +0700

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = ""; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzgXkxSgfM_7lEEfxtAwKxffrAGSmeumZoB_UyX5MNyQyEuv9jS_vAR0hypgUeRUkPhMA/exec"; 
 
 export interface FormField {
   id: string;
