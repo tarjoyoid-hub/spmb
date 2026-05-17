85c92ce14a91bee3dab335f63996169229b4b645
tarjoyoid-hub <tarjoyo.id@gmail.com>
2026-05-17 18:42:37 +0700

    Update CheckStatus.tsx

--- src/pages/CheckStatus.tsx
+++ src/pages/CheckStatus.tsx
@@ -58,7 +58,7 @@ export default function CheckStatus() {
         
         doc.setFontSize(16);
         doc.setFont('helvetica', 'bold');
-        doc.text('BUKTI KETERANGAN LULUS PENDAFTARAN MURID BARU', 105, currentY, { align: 'center' });
+        doc.text('SURAT KETERANGAN', 105, currentY, { align: 'center' });
         currentY += 8;
         doc.setFontSize(12);
         doc.setFont('helvetica', 'normal');
@@ -74,7 +74,7 @@ export default function CheckStatus() {
         console.error("Error adding kop surat", e);
         doc.setFontSize(16);
         doc.setFont('helvetica', 'bold');
-        doc.text('LULUS PENDAFTARAN MURID BARU', 105, currentY, { align: 'center' });
+        doc.text('SURAT KETERANGAN', 105, currentY, { align: 'center' });
         currentY += 8;
         doc.setFontSize(12);
         doc.setFont('helvetica', 'normal');
