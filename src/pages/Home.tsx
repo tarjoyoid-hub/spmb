348dfd4f116a3e3958315b80caf671a09b2cf586
tarjoyoid-hub <tarjoyo.id@gmail.com>
2026-05-18 05:41:44 +0700

    Update welcome message based on isClosed state

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -48,7 +48,7 @@ export default function Home() {
                 {!isClosed && <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-400 opacity-75"></span>}
                 <span className={`relative inline-flex rounded-full h-3 w-3 ${isClosed ? 'bg-red-500' : 'bg-blue-500'}`}></span>
               </span>
-              {isClosed ? `Selamat Datang! SPMB ${new Date().getFullYear()} Telah Ditutup` : `Selamat Datang! SPMB ${new Date().getFullYear()} Telah Dibuka`}
+              {isClosed ? `Mohon Maaf!  SPMB ${new Date().getFullYear()} Telah Ditutup` : `Selamat Datang!  SPMB ${new Date().getFullYear()} Telah Dibuka`}
             </motion.div>
             
             <motion.h1
